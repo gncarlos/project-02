0f332f432a7bdc96b51f2986994a1f15bbfd92a7
Carlos Gonzalez <carlos.gonzalez_arce@spaceforce.mil>
2021-07-06 10:56:48 -0600

    added struture changes before lunch

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+//import Nav from './components/Nav.js';
+
+function App() {
+  return (
+    <div >
+      Hello
+      {/* <Nav/> */}
+
+    </div>
+  );
+}
+
+export default App;
